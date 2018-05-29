b011f300e362ff427fb0082d9b23a0bb7a4ce196
Sonia <soniatoledo294@hotmail.com>
2018-05-28 21:05:07 -0400

    Promises finished

--- http/promesas3.js
+++ http/promesas3.js
@@ -1,3 +1,4 @@
+ 
 /*
  * Esto es un borrador de JavaScript.
  *
@@ -6,6 +7,16 @@
  * 2. Inspeccionar para mostrar el inspector de objetos con el resultado (Ctrl+I), o,
  * 3. Mostrar para insertar el resultado en un comentario después de la selección. (Ctrl+L)
  */
+
+
+function showMessage(  ar){
+  
+  var el= document.createElement("div");
+  el.textContent=  ar;
+  return document.body.appendChild( el);
+}
+
+
 
  function getJSON(url){
    
@@ -17,45 +28,47 @@
      req.addEventListener("load", function(){
        if(req.status < 400 ){
          console.log("exito" );
+         console.log(   req.responseText )  ;
          var obj= JSON.parse(  req.responseText );
          exito( obj  );
-         //console.log(   req.responseText )  ;
+         
        }else{
-         console.log("falla");
+         console.log("Falla");
          console.log ("Request failed "+req.statusText) ;
        }
        
      });
      
-      req.addEventListener("error", function(){
-       console.log(   "Network error " ); 
-      });
+      req.addEventListener("error", function(){ console.log(   "Network error " );  });
      
      
      req.send( null );
-   });
- }
+   });/** Fin promesa **/
+ }/** Fin funcion ***/
 
 
 
 
-function showMessage(  ar){
-  
-  var el= document.createElement("div");
-  el.textContent=  ar;
-  return document.body.appendChild( el);
-}
 
 
 var loading= showMessage("Loading ... (..)");
 
 
-getJSON("http://localhost/learn-php/Peticiones/").then( 
-function(param_success){
-  
-  
-  
-  
+var mother= getJSON("res/bert.json").then( 
+function(   bert   ){      return getJSON(  bert.spouse   );//RETORNA OTRA PROMESA
+}).then(     function(sposo){
+    //retornar otra promesa 
+    return getJSON(  sposo.mother  );
+}).then(     function(mother){//ver nombre de la madre
+    
+    showMessage("The mother name is "+   mother.name );
+}).catch(function(error){
+  showMessage(  String(  error) );
+})
+.then( function(){
+  document.body.removeChild(  loading  );
 });
 
+console.log(  typeof  mother);
 
+ 
